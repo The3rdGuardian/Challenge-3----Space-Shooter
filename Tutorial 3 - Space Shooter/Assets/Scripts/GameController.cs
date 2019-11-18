861897c485019be3f17f987fcded6bff89d78278
Edward <edwardddpowers3@knights.ucf.edu>
2019-11-17 23:17:06 -0500

    Finished Challenge 3

---
+++ Tutorial 3 - Space Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] hazards;
+    public GameObject Player;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public Text ScoreText;
+    public Text restartText;
+    public Text gameOverText;
+    public Text winText;
+
+
+    private int score;
+    private bool restart;
+    private bool gameOver;
+
+    void Start()
+    {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        winText.text = "";
+        score = 0;
+       
+        StartCoroutine(SpawnWaves());
+        UpdateScore();
+    }
+
+    void Update()
+    {
+        if(restart)
+        {
+            if(Input.GetKeyDown(KeyCode.F))
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while(true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if(gameOver)
+            {
+                restartText.text = "Press 'F' for Restart";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!                      " +
+            " Game Created by Edward Powers";
+        gameOver = true;
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        newScoreValue = 15;
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        ScoreText.text = "Points: " + score;
+
+        if(score >= 100)
+        {
+            GameOver();
+            winText.text = "You Win!";
+            gameOver = true;
+            restart = true;
+        }
+    }
+}
